5823b6c84d75540c75edf2299eccf8e2076f74f8
abdullahazab <abdullah.a.azab@gmail.com>
2020-01-12 15:48:41 +0200

    Updates

---
+++ precache-manifest.9c6a1a597973c3ea662833b69f50c12a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18dcccbef5ca95abc4271cc3d169c132",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "592afb2e4bb61792c7e5",
+    "url": "/myapp/static/css/main.9ff5f9bc.chunk.css"
+  },
+  {
+    "revision": "4a57acee3e4ba709365b",
+    "url": "/myapp/static/js/2.35db95e7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myapp/static/js/2.35db95e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "592afb2e4bb61792c7e5",
+    "url": "/myapp/static/js/main.a34c3144.chunk.js"
+  },
+  {
+    "revision": "4c5d71b77e57f8b73954",
+    "url": "/myapp/static/js/runtime-main.16e32a26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
